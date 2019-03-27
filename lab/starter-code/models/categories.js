00653beabd2e3dd27c02590e9dfda3363367acc1
Erin Trainor <trainor.erin@gmail.com>
2019-03-27 15:18:10 -0700

    file setup

---
+++ lab/starter-code/models/categories.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const uuid = require('uuid/v4');
+
+class Categories {
+
+  constructor() {
+    this.database = [];
+  }
+
+  get(_id) {
+  }
+  
+  post(record) {
+  }
+
+  put(_id, record) {
+  }
+
+  delete(_id) {
+  }
+
+}
+
+module.exports = Categories;
